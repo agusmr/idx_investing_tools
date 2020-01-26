99155ffdeea4e4f2ba2fb5f1f55c2bc7a8a4f388
kevin janada <kevin.janada@gmail.com>
2020-01-26 20:50:58 +0700

    Create get-reports cli command

--- main.go
+++ main.go
@@ -1,90 +1,82 @@
 package main
 
-import ( // "time"
+import (
+	"flag"
 	"fmt"
-	"time"
-
-	"github.com/kevinjanada/idx_investing_tools/constants"
-	"github.com/kevinjanada/idx_investing_tools/services"
+	"os"
+	//"time"
+	//"github.com/kevinjanada/idx_investing_tools/constants"
+	//"github.com/kevinjanada/idx_investing_tools/services"
 )
 
+var commands = []string{
+	"get-reports",
+}
+
 func main() {
-	// statementService := services.NewStatementService("api_development")
-	// err := statementService.NewStatement(constants.StatementChangesInEquity)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	// SubCommands -------
+	//  get-reports subcommand
+	getReportsCommand := flag.NewFlagSet("get-reports", flag.ExitOnError)
+	//  get-reports flags
+	getReportsYearPtr := getReportsCommand.Int(
+		"year",
+		0,
+		"Which financial year reports to download. Currently available {2017|2018|2019}",
+	)
 
-	// testFile, err := services.NewExcelReport("files/excel_reports/2017/trimester_3/FinancialStatement-2017-III-AALI.xlsx")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// preferredStockValue := testFile.PreferredStock()
-	// fmt.Println(preferredStockValue)
-
-	excelReports, err := services.OpenExcelFilesInDir("files/excel_reports/2017/trimester_3")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	statementService := services.NewStatementService("api_development")
-	// rowTitleID, err := uuid.FromBytes([]byte("18374f1e-d432-4058-8597-a8ef471eecdf"))
-	// stockID, err := uuid.FromBytes([]byte("b227b28b-3139-458a-b38d-11c64ec1f9f1"))
-	// date, err := time.Parse(time.RFC3339, "2017-09-30T00:00:00Z")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// statementRow := statementService.StatementRowExists(rowTitleID, stockID, date)
-	// fmt.Printf("%+v", statementRow)
-	// err = statementService.NewStatementRowTitle(constants.RowPreferredStocks)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	location, err := time.LoadLocation("Asia/Jakarta")
-	for _, report := range excelReports {
-		stockCode := report.EntityCode()
-		stockPreferredStock := report.PreferredStock()
-		fmt.Printf("%s preferred-stock: %f\n", stockCode, stockPreferredStock)
-		date := time.Date(2017, 9, 30, 0, 0, 0, 0, location)
-
-		// Insert Preferred Stock
-		err = statementService.InsertUpdateStatementRow(
-			stockCode,
-			constants.StatementChangesInEquity,
-			constants.RowPreferredStocks,
-			stockPreferredStock,
-			date,
-		)
-		if err != nil {
-			fmt.Println(err)
+	// No Subcommands error handler
+	if len(os.Args) < 2 {
+		//fmt.Println("")
+		fmt.Println("Need to input commands")
+		fmt.Println("   Available commands:")
+		for _, c := range commands {
+			fmt.Printf("          %s\n", c)
 		}
-
-		// stockTotalAssets := report.TotalAssets()
-		// // Insert Total Assets
-		// err = statementService.InsertUpdateStatementRow(
-		// 	stockCode,
-		// 	constants.StatementFinancialPosition,
-		// 	constants.RowTotalAssets,
-		// 	stockTotalAssets,
-		// 	date,
-		// )
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-
-		// stockNetIncome := report.NetIncome()
-		// // Insert Net Income
-		// err = statementService.InsertUpdateStatementRow(
-		// 	stockCode,
-		// 	constants.StatementProfitOrLoss,
-		// 	constants.RowNetIncome,
-		// 	stockNetIncome,
-		// 	date,
-		// )
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
+		os.Exit(1)
 	}
+
+	// Parse Subcommands
+	switch os.Args[1] {
+	case "get-reports":
+		getReportsCommand.Parse(os.Args[2:])
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// Handle get-reports subcommand
+	if getReportsCommand.Parsed() {
+		if *getReportsYearPtr == 0 {
+			getReportsCommand.PrintDefaults()
+			os.Exit(1)
+		}
+		fmt.Println("Download", *getReportsYearPtr)
+	}
+
+	//excelReports, err := services.OpenExcelFilesInDir("files/excel_reports/2017/trimester_3")
+	//if err != nil {
+	//fmt.Println(err)
+	//}
+
+	//statementService := services.NewStatementService("api_development")
+
+	//location, err := time.LoadLocation("Asia/Jakarta")
+	//for _, report := range excelReports {
+	//stockCode := report.EntityCode()
+	//stockPreferredStock := report.PreferredStock()
+	//fmt.Printf("%s preferred-stock: %f\n", stockCode, stockPreferredStock)
+	//date := time.Date(2017, 9, 30, 0, 0, 0, 0, location)
+
+	//// Insert Preferred Stock
+	//err = statementService.InsertUpdateStatementRow(
+	//stockCode,
+	//constants.StatementChangesInEquity,
+	//constants.RowPreferredStocks,
+	//stockPreferredStock,
+	//date,
+	//)
+	//if err != nil {
+	//fmt.Println(err)
+	//}
+	//}
 }
